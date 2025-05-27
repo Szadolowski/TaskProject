1cb1c57b4bf89df9924727d77249c8960dca8eaf
Rafal Curzydlo <rafall.curzydlo@gmail.com>
2025-05-27 21:59:25 +0200

    0.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,49 @@
 import ProjectSidebar from "./components/ProjectSidebar";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
+import { useState } from "react";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStatAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleAddProject(ProjectData) {
+    setProjectState((prevState) => {
+      const newProject = {
+        ...ProjectData,
+        id: Math.random(),
+      };
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  console.log(projectState);
+
+  let content;
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStatAddProject} />;
+  }
+
   return (
     <main className="flex h-screen my-8 gap-8">
-      <ProjectSidebar />
-      <NoProjectSelected />
+      <ProjectSidebar OnStartAddProject={handleStatAddProject} />
+      {content}
     </main>
   );
 }
